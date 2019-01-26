b9b7db70cce434982d31b4bc797264cae7e0d4df
David Tucker <dtucker@i-neda.com>
2019-01-26 20:37:54 +0000

    Initial commit.

---
+++ query_builder/query_builder.go
@@ -0,0 +1,74 @@
+package query_builder
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/jinzhu/inflection"
+)
+
+type QueryBuilder struct{}
+
+// NewQueryBuilder returns a pointer to a new instance of the QueryBuilder struct.
+func NewQueryBuilder() *QueryBuilder {
+	return &QueryBuilder{}
+}
+
+// BuildInsertQuery constructs and returns a MySQL INSERT query from the passed object.
+func (builder *QueryBuilder) BuildInsertQuery(object interface{}) string {
+	return strings.Join([]string{
+		"INSERT INTO",
+		builder.getTableName(object),
+		"(" + strings.Join(builder.getColumnNames(object), ",") + ")",
+		"VALUES",
+		"(" + strings.Join(builder.getValues(object), ",") + ")",
+	}, " ")
+}
+
+// BuildInsertQuery constructs and returns a MySQL UPDATE query from the passed object.
+func (builder *QueryBuilder) BuildUpdateQuery(object interface{}) string {
+	return strings.Join([]string{
+		"UPDATE",
+		builder.getTableName(object),
+		"SET",
+		builder.buildUpdateValues(object),
+		"WHERE",
+		builder.getPrimaryKeyColumnName(object) + "=?",
+	}, " ")
+}
+
+func (builder *QueryBuilder) getTableName(object interface{}) string {
+	typ := reflect.TypeOf(object).Elem()
+	return inflection.Plural(typ.Name())
+}
+
+func (builder *QueryBuilder) getColumnNames(object interface{}) []string {
+	typ := reflect.TypeOf(object).Elem()
+	columnNames := make([]string, 0)
+	for i := 0; i < typ.NumField(); i++ {
+		columnNames = append(columnNames, typ.Field(i).Name)
+	}
+	return columnNames
+}
+
+func (builder *QueryBuilder) getValues(object interface{}) []string {
+	typ := reflect.TypeOf(object).Elem()
+	values := make([]string, 0)
+	for i := 0; i < typ.NumField(); i++ {
+		values = append(values, "?")
+	}
+	return values
+}
+
+func (builder *QueryBuilder) buildUpdateValues(object interface{}) string {
+	typ := reflect.TypeOf(object).Elem()
+	values := make([]string, 0)
+	for i := 0; i < typ.NumField(); i++ {
+		values = append(values, typ.Field(i).Name+"=?")
+	}
+	return strings.Join(values, ",")
+}
+
+func (builder *QueryBuilder) getPrimaryKeyColumnName(object interface{}) string {
+	return "Id"
+}
---
+++ query_builder/query_builder_test.go
@@ -0,0 +1,42 @@
+package query_builder_test
+
+import (
+	. "github.com/dtucker2/database/query_builder"
+
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueryBuilder_BuildInsertQuery(t *testing.T) {
+	t.Run("basic", func(t *testing.T) {
+		type Object struct {
+			Id        int
+			Name      string
+			CreatedAt *time.Time
+			UpdatedAt *time.Time
+		}
+		object := Object{
+			Name: "Test Object",
+		}
+		builder := NewQueryBuilder()
+		assert.Equal(t, `INSERT INTO Objects (Id,Name,CreatedAt,UpdatedAt) VALUES (?,?,?,?)`, builder.BuildInsertQuery(&object))
+	})
+}
+
+func TestQueryBuilder_BuildUpdateQuery(t *testing.T) {
+	t.Run("basic", func(t *testing.T) {
+		type Object struct {
+			Id        int
+			Name      string
+			CreatedAt *time.Time
+			UpdatedAt *time.Time
+		}
+		object := Object{
+			Name: "Test Object",
+		}
+		builder := NewQueryBuilder()
+		assert.Equal(t, `UPDATE Objects SET Id=?,Name=?,CreatedAt=?,UpdatedAt=? WHERE Id=?`, builder.BuildUpdateQuery(&object))
+	})
+}
